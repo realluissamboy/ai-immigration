12ef711b9353c65edc6dea6700af38e8b7e81754
Luis Samboy <luissamboy@icloud.com>
2023-09-25 23:55:36 -0400

    Created a ts and json files with 20 us civics questions

---
+++ src/components/questions.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1 className="text-rose-500 text-xs italic	">This is a header</h1>
+    </div>
+  )
+}
---
+++ src/data/questions.ts
@@ -0,0 +1,84 @@
+const civicsQuestions: { question: string; answer: string }[] = [
+  {
+    question: 'What is the supreme law of the land?',
+    answer: 'The Constitution',
+  },
+  {
+    question: 'What does the Constitution do?',
+    answer: 'Sets up the government, defines the government, protects basic rights of Americans.',
+  },
+  {
+    question: 'What is the highest court in the United States?',
+    answer: 'The Supreme Court',
+  },
+  {
+    question: 'What are the first three words of the Constitution?',
+    answer: 'We the People',
+  },
+  {
+    question: 'What do we call the first ten amendments to the Constitution?',
+    answer: 'The Bill of Rights',
+  },
+  {
+    question: 'How many amendments does the Constitution have?',
+    answer: '27 amendments',
+  },
+  {
+    question: 'What did the Declaration of Independence do?',
+    answer: 'Declared our independence (from Great Britain)',
+  },
+  {
+    question: 'What are two rights in the Declaration of Independence?',
+    answer: 'Life, liberty',
+  },
+  {
+    question: 'What is freedom of religion?',
+    answer: 'You can practice any religion, or not practice a religion.',
+  },
+  {
+    question: 'What is the economic system in the United States?',
+    answer: 'Capitalism economy',
+  },
+  {
+    question: "What is the 'rule of law'?",
+    answer: 'Everyone must follow the law. Leaders must obey the law.',
+  },
+  {
+    question: 'What stops one branch of government from becoming too powerful?',
+    answer: 'Checks and balances',
+  },
+  {
+    question: 'Who is in charge of the executive branch?',
+    answer: 'The President',
+  },
+  {
+    question: 'Who makes federal laws?',
+    answer: 'Congress (the Senate and House of Representatives)',
+  },
+  {
+    question: 'What are the two parts of the U.S. Congress?',
+    answer: 'The Senate and the House of Representatives',
+  },
+  {
+    question: 'How many U.S. Senators are there?',
+    answer: '100 Senators',
+  },
+  {
+    question: "Who is one of your state's U.S. Senators now?",
+    answer: "This will vary depending on your state. Please check your state's current Senators.",
+  },
+  {
+    question: 'The House of Representatives has how many voting members?',
+    answer: '435 voting members',
+  },
+  {
+    question: 'Why do some states have more Representatives than other states?',
+    answer: "Because of the state's population",
+  },
+  {
+    question: 'What is one power of the federal government?',
+    answer: 'To print money',
+  },
+]
+
+export default civicsQuestions
