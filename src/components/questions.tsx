6c415ec99bab746ea069bc57847a18fd9b0e1362
Luis Samboy <luissamboy@icloud.com>
2023-09-26 00:24:24 -0400

    Create useState and useEffect for answers

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -2,16 +2,27 @@ import { useState, useEffect } from 'react'
 import civicsQuestions from '../data/questions'
 
 export default function Questions() {
-  const [currentQuestion, setCurrentQuestion] = useState<string>('The current question is...')
+  const [currentQuestion, setCurrentQuestion] = useState<{
+    question: string
+    answer: string
+  } | null>(null)
+  const [userAnswer, setUserAnswer] = useState<string>('')
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * civicsQuestions.length)
-    setCurrentQuestion(civicsQuestions[randomIndex].question)
+    setCurrentQuestion(civicsQuestions[randomIndex])
   }, [])
+
+  const handleSubmit = () => {
+    if (currentQuestion) {
+    }
+  }
 
   return (
     <div>
-      <p>{currentQuestion}</p>
+      <h2>{currentQuestion?.question}</h2>
+      <input type="text" value={userAnswer} onChange={(e) => setUserAnswer(e.target.value)} />
+      <button onClick={handleSubmit}></button>
     </div>
   )
 }
