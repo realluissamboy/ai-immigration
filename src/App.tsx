87292f9387344b83f12d85534420167362aedad5
Luis Samboy <luissamboy@icloud.com>
2023-09-27 18:01:38 -0400

    Elevated questions.tsx to main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react'
+import civicsQuestions from './data/questions'
 import './App.css'
-import Questions from './components/questions'
 import FeedbackComponent from './components/feedback'
 
 export default function App() {
+  const [currentQuestion, setCurrentQuestion] = useState<{
+    question: string
+    answer: string
+  } | null>(null)
+  const [userAnswer, setUserAnswer] = useState<string>('')
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * civicsQuestions.length)
+    setCurrentQuestion(civicsQuestions[randomIndex])
+  }, [])
+
+  const handleSubmit = () => {
+    if (currentQuestion) {
+    }
+  }
+
   return (
     <div>
-      <Questions />
+      <h2>{currentQuestion?.question}</h2>
+      <input type="text" value={userAnswer} onChange={(e) => setUserAnswer(e.target.value)} />
+      <button onClick={handleSubmit}>Submit Button</button>
       <FeedbackComponent />
     </div>
   )
